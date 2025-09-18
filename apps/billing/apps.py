1040f7ad4c6d2c00938cafce1c9d2100e770ca23
Morthala Srija <srija898@gmail.com>
2025-09-18 22:21:28 +0530

    Created new super user

--- apps/billing/apps.py
+++ apps/billing/apps.py
@@ -1,8 +1,16 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+import os
 
-class BillingConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.billing"
+class BillingEngineConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'billing_engine'
 
     def ready(self):
-        from . import signals  # noqa
+        User = get_user_model()
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="Srija123",
+                email="srijamorthala104@gmail.com",
+                password="django123"
+            )
